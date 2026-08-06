44ed006f71b249862a561cdbb9d9ef279eda5a82
Amritesh Gautam <amriteshx18@gmail.com>
2026-08-06 14:04:46 +0530

    Implement palindrome checking in loc125.java

---
+++ Day4/loc125.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            char a = s.charAt(i);
+            char b = s.charAt(j);
+
+            if (!Character.isLetterOrDigit(a)) {
+                i++;
+            } else if (!Character.isLetterOrDigit(b)) {
+                j--;
+            } else {
+                if (Character.toLowerCase(a) != Character.toLowerCase(b)) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+    }
+}
