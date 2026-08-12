12440c11fb9a9deb07bf3d152af38f5dcdb1eea1
Amritesh Gautam <amriteshx18@gmail.com>
2026-08-12 16:27:06 +0530

    Implement intersection method to find common elements between two arrays

---
+++ Day8/loc349.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> resultSet = new HashSet<>();
+
+        // all elements of nums1 to set
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        // for intersection
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        // set to array
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+}
